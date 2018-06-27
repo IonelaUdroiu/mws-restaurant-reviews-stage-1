50494cb65c8db3fd3fab540ba1843a7687ef8b0e
IonelaUdroiu <37430418+IonelaUdroiu@users.noreply.github.com>
2018-06-28 02:51:16 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Serviceworker name and version
 let cacheID = 'restaurant-reviews-001';
 // Data to be cached
-let urlsToCache = ['/', './index.html', './restaurant.html', './js/', '/js/dbhelper.js', '/js/main.js', '/js/register.js', '/js/restaurant_info.js', '/css/styles.css', '/data/restaurants.json', '/img/', '/img/1.jpg', '/img/2.jpg', '/img/3.jpg', '/img/4.jpg', '/img/5.jpg', '/img/6.jpg', '/img/7.jpg', '/img/8.jpg', '/img/9.jpg', '/img/10.jpg'];
+let urlsToCache = ['/', './index.html', 'restaurant.html', './js/', '/js/dbhelper.js', '/js/main.js', '/js/register.js', '/js/restaurant_info.js', '/css/styles.css', '/data/restaurants.json', '/img/', '/img/1.jpg', '/img/2.jpg', '/img/3.jpg', '/img/4.jpg', '/img/5.jpg', '/img/6.jpg', '/img/7.jpg', '/img/8.jpg', '/img/9.jpg', '/img/10.jpg'];
 //Install the serviceworker and open a new cache
 self.addEventListener('install', function(event) {
 	console.log('Serviceworker is installing');
